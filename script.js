5913ee14d1537b8963ccd34aa11192f569857a48
Valen Hk <valenhk86@gmail.com>
2026-02-11 15:28:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Demo Coliseum Fitness Center cargada correctamente");
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const header = document.querySelector(".main-header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+
+// - animaciones
+// - scroll suave
+// - seccines interactivas
